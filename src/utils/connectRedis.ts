a5f7e70d070cadcc33b339d2f03d6886b6f5ee09
zamanien <tari0048@stud.kea.dk>
2022-12-28 15:28:59 +0100

    modified auth to use hosted DBs

--- src/utils/connectRedis.ts
+++ src/utils/connectRedis.ts
@@ -2,7 +2,6 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const redisUrl = `redis://localhost:6380`;
 const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
