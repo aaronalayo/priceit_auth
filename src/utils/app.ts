ce53434b6fbdd89fc3e2ef8f3f23b15afb4cfa28
zamanien <tari0048@stud.kea.dk>
2023-01-13 22:55:32 +0100

    cleanup and

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -6,25 +6,22 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRouter from '../routes/user.route';
 import authRouter from '../routes/auth.route';
-import { rateLimit } from 'express-rate-limit';
-
-// import { config } from '../../config/custom-environment-variables';
 
 const app = express();
 // app.use(rateLimit)
 
+// Middleware
+
 // 1. Cors
 app.use(
   cors({
-    origin: config.get<string>('origin'),
+    origin: config.get<string>('prod_origin'),
     // origin: config.auth.origin,
     credentials: true,
   })
 );
 
-// Middleware
-
-// 1. Body Parser
+// 2. Body Parser
 app.use(express.json({ limit: '10mb' }));
 
 // 3. Cookie Parser
@@ -32,6 +29,7 @@ app.use(cookieParser());
 
 // 4. Logger
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 // 5. Routes
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
@@ -44,7 +42,7 @@ app.get('/healthChecker', (req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-// UnKnown Routes
+// Unknown Routes
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
@@ -62,9 +60,4 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-// const port = config.get<number>('port');
-// app.listen(port, () => {
-//   console.log(`Server started on port: ${port}`);
-//   mongoose();
-// });
 export default app;
