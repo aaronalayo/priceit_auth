77a764b499ca274c28db473b58778fa8b1187fc4
zamanien <tari0048@stud.kea.dk>
2023-01-15 03:52:00 +0100

    Cors updating

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -25,14 +25,6 @@ app.set('trust proxy', 1)
 // 1. Cors
 app.use(cors({ origin: ['https://priceit.zamanien.com', 'http://localhost:5173'], credentials: true }));
 
-app.use((req, res, next) => {
-  const cloudflareIps = ['103.21.244.0/22', '103.22.200.0/22', '103.31.4.0/22', '104.16.0.0/12', '108.162.192.0/18', '131.0.72.0/22', '141.101.64.0/18', '162.158.0.0/15', '172.64.0.0/13', '173.245.48.0/20', '188.114.96.0/20', '190.93.240.0/20', '197.234.240.0/22', '198.41.128.0/17', '104.21.36.183:443'];
-  if (cloudflareIps.includes(req.ip)) {
-    next();
-  } else {
-    res.status(403).send('Forbidden');
-  }
-});
 // 2. Body Parser
 app.use(express.json({ limit: '10mb' }));
 
